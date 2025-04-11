baf06b2de33a0e23e854dd528a6c931cf7d1d878
장태문 <rhffkfk@naver.com>
2025-04-11 18:32:42 +0900

    ✨django form 사용, 데이터 받아오기

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,11 @@ from django.template import loader
 from django.shortcuts import render #단축 기능(shortcuts)제공
 from django.http import Http404 #Http404 예외 발생
 from django.shortcuts import get_object_or_404 #Django Http404 기능에 대한 단축 기능
+from django.http import HttpResponseRedirect # vote() 가상
+from django.urls import reverse # vote() 가상
 
 from .models import Question
+from .models import Choice # vote() 가상
 
 # Create your views here.
 
@@ -47,8 +50,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
